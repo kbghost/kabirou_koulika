ca52f1a8acd154b35f586fa9d2ae31cfdbe0002c
kabirou koulika <kabiroukoulika@gmail.com>
2026-07-21 18:27:06 +0100

    Preparation du projet pour Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = '/kabirou_koulika'; // Corriger ici avec le vrai nom de ton dépôt !
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Requis pour l'export statique sur GitHub Pages
-    remotePatterns: [
-      { protocol: "https", hostname: "github.com" },
-      { protocol: "https", hostname: "avatars.githubusercontent.com" },
-      { protocol: "https", hostname: "raw.githubusercontent.com" },
-    ],
-  },
-  basePath: isProd ? repoName : '',
-  assetPrefix: isProd ? `${repoName}/` : '',
+  /* plus besoin de basePath, assetPrefix ou output: export */
 };
 
 export default nextConfig;
